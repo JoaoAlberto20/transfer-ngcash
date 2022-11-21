8df36e5e43cb4d54e3eeadfc6ec1ba9f9312c5d2
João Alberto <joaoalbertosv15@gmail.com>
2022-11-21 18:21:56 -0300

    fea: Adicionando autenticação na rota de balance

--- app/backend/src/routes/account.routes.ts
+++ app/backend/src/routes/account.routes.ts
@@ -2,16 +2,14 @@ import { Router } from 'express';
 import { AccountController } from '@controller/AccountController';
 import { AccountService } from '@services/AccountService';
 import { ensureAuthenticated } from '@shared/middleware/ensureAuthenticated';
+import { App } from 'api/app';
 
 
 const accountController = new AccountController(new AccountService());
 
 const accountRoutes = Router();
 
-accountRoutes.get(
-  '/',
-  ensureAuthenticated ,
-  accountController.getBalance
-);
+accountRoutes.use( ensureAuthenticated);
+accountRoutes.get('/', accountController.getBalance);
 
 export { accountRoutes };
